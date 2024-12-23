ca0562f79f9e48daca538c4c0bf1f4ac1a85b3e8
naysaralodi <naysaralodi@gmail.com>
2024-12-23 10:58:47 -0300

    fix: add components/button follow

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,10 @@
+import TwitterFollowButton from "./button-follow";
+
 const Header: React.FC = () => {
     return (
       <header className="bg-blue-500 text-white p-4 text-center inline-flex flex-row justify-between w-full">
         <h1 className="text-2xl font-bold">Welcome to MesaScout</h1>
-        <a href="https://twitter.com/Mesascout?ref_src=twsrc%5Etfw" className="twitter-follow-button" data-show-count="false">Follow @Mesascout</a><script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+        <TwitterFollowButton />
       </header>
     );
   };
