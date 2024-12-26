810b18b7b383d700c5cbdf91b0ded83327eab105
naysaralodi <naysaralodi@gmail.com>
2024-12-26 12:29:13 -0300

    fix: ajusted errors netlify

--- src/pages/api/decryptFile.ts
+++ src/pages/api/decryptFile.ts
@@ -1,8 +1,10 @@
 import { decryptFile } from '../../utils/decrypt';
 
-const handler = async (req, res) => {
-  const decryptedData = await decryptFile();
-  res.status(200).json(decryptedData);
-};
-
-export default handler;
+export default async function handler(req, res) {
+  try {
+    const decryptedData = await decryptFile();
+    res.status(200).json(decryptedData);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to decrypt the file.' });
+  }
+}
