eeedd5c98631d1544f60b180102bf00a13f6bb01
naysaralodi <naysaralodi@gmail.com>
2025-01-15 12:43:48 -0300

    ADC new filters at table

--- src/components/thead.tsx
+++ src/components/thead.tsx
@@ -31,7 +31,12 @@ const THead: React.FC<THeadProps> = ({ sortField, sortOrder, onSort }) => {
         <th className="border border-gray-300 px-4 py-2">Name</th>
         <th className="border border-gray-300 px-4 py-2">Position</th>
         <th className="border border-gray-300 px-4 py-2">Games</th>
-        <th className="border border-gray-300 px-4 py-2">Age</th>
+        <th 
+          className="border border-gray-300 px-4 py-2 cursor-pointer"
+          onClick={() => onSort("age")}
+          >
+            Age {renderSortIcon("age")}
+        </th>
         <th
           className="border border-gray-300 px-4 py-2 cursor-pointer"
           onClick={() => onSort("score")}
