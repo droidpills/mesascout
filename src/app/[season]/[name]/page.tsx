55601793c2cad56804462ddc8bb09156c6a613a1
naysaralodi <naysaralodi@gmail.com>
2025-02-05 18:16:49 -0300

    Feat: layout intern page player part 1

--- src/app/[season]/[name]/page.tsx
+++ src/app/[season]/[name]/page.tsx
@@ -4,8 +4,7 @@ import { notFound } from 'next/navigation';
 import { Player } from "../../types/Player";
 import { normalizeName } from "../../utils/normalizeName";
 import { normalizeFileName } from "../../utils/normalizeFileName";
-import { FaWhatsapp } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";
+import usa from "../../../../public/images/usa.png";
 
 const SEASONS_DATA = {
   season24: "https://storage.googleapis.com/mesascout/players_with_positions.json",
@@ -35,9 +34,11 @@ export async function generateStaticParams() {
 }
 
 interface PlayerDetailsProps {
-  params: Promise <{season: string;
-    name: string;}>
-  };
+  params: Promise<{
+    season: string;
+    name: string;
+  }>
+};
 
 export default async function PlayerDetails({ params }: PlayerDetailsProps) {
   const { season, name } = await params;
@@ -91,6 +92,20 @@ export default async function PlayerDetails({ params }: PlayerDetailsProps) {
       },
     ];
 
+    const statistics = [
+      {
+        label: 'Idade',
+        value: `${player.age}`,
+      },
+      {
+        label: 'Jogos',
+        value: `${player.games}`,
+      },
+      {
+        label: 'Valor',
+        value: `${player.value}`,
+      },
+    ];
     return (
       <div>
         <Head>
@@ -103,70 +118,68 @@ export default async function PlayerDetails({ params }: PlayerDetailsProps) {
           <meta name="twitter:card" content="summary_large_image" />
         </Head>
 
-        <main className="p-4">
-          <div className="flex items-center">
-            <Image
-              src={playerImageURL}
-              alt={`${player.name} Foto`}
-              width={200}
-              height={200}
-              className="rounded-lg"
-              style={{ height: "auto" }} 
-            />
-            <h2 className="text-2xl font-bold mb-4 ml-4">{player.name}</h2>
-          </div>
-          <table className="table-auto border-collapse border border-gray-300 w-full mt-4">
-            <tbody>
-              {playerAttributes.map(({ label, value }) => (
-                <tr key={label}>
-                  <td className="border border-gray-300 px-4 py-2 font-bold">{label}</td>
-                  <td className="border border-gray-300 px-4 py-2">{value}</td>
-                </tr>
+        {/* <main className="p-4"> */}
+        <div className="mx-auto w-[380px] flex items-center justify-center h-screen p-5">
+          <div className="rounded-3xl border border-custom-gray-200 bg-gray-100">
+            <div className="rounded-3xl p-4 ring-1 ring-gray-200">
+              <div className="relative overflow-hidden pb-3">
+                <div className="overflow-hidden [filter:url('#rounded')]">
+                  <div className="relative h-[400px] border border-custom-gray-200 bg-gradient-to-b from-orange-600 to-yellow-500 [clip-path:polygon(0_0,_100%_0,_100%_95%,_50%_100%,_0_95%)]">
+                    <div className="pointer-events-none absolute start-1/2 top-10 -z-10 ms-8 -translate-x-1/2 text-center text-9xl/[0.8em] font-extrabold uppercase italic tracking-tighter text-white opacity-40 mix-blend-overlay">
+                      <div>{player.name}</div>
+                      <div>{player.name}</div>
+                    </div>
+                    {/* <Image
+                      src={playerImageURL}
+                      alt={`${player.name} Foto`}
+                      width={500}
+                      height={600}
+                      className="absolute start-1/2 top-2 max-w-[calc(100%+60px)] -translate-x-1/2 z-0"
+                    /> */}
+                  </div>
+                </div>
+
+                <div className="absolute bottom-0 start-1/2 flex h-12 w-12 -translate-x-1/2 items-center justify-center rounded-2xl bg-gradient-to-b from-[#ff5411] to-[#fc0] text-2xl/none font-extrabold tracking-tighter text-white">
+                  {player.score}
+                </div>
+
+                <div className="absolute start-0 top-0 aspect-square w-[76px] -translate-x-1/2 -translate-y-1/2 rounded-full border border-custom-gray-200 bg-white">
+
+                </div>
+
+                <div className="absolute top-0 right-0 aspect-square w-[76px] -translate-y-1/2 translate-x-1/2 rounded-full border border-custom-gray-200 bg-white flex items-center justify-center">
+                  
+                </div>
+
+              </div>
+
+              <div className="pb-1 pt-3 text-center text-slate-800 ">
+                <h2 className="text-[22px]/tight font-bold tracking-tight">{player.name}</h2>
+                <div className="text-sm">{player.club}</div>
+              </div>
+            </div>
+
+            <div className="mx-auto grid w-fit grid-cols-3 divide-x divide-custom-gray-200 py-5 text-slate-800">
+              {statistics.map((statistic) => (
+                <div key={statistic.label} className="px-7 text-center">
+                  <div className="mb-2 text-sm/tight font-bold">{statistic.value}</div>
+                  <div className="text-[0.6875rem]/tight uppercase">{statistic.label}</div>
+                </div>
               ))}
-            </tbody>
-          </table>
-          <div className="flex items-center justify-center p-10">
-            <h3 className="p-5">
-              <b>Mapa de calor:</b>
-            </h3>
-            <Image
-              src={heatmapImageURL}
-              alt={`${player.name} Heatmap`}
-              width={400}
-              height={200}
-              className="rounded-lg"
-              style={{ height: "auto" }} 
-            />
+            </div>
           </div>
 
-          <div className="flex items-center justify-center p-10">
-            <h3 className="p-5">
-              <b>Melhores momentos:</b>
-            </h3>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/C8HUbFKkeRc?si=wwnQstx1nxZ7ghhZ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-          </div>
-
-          <div className="mt-8 flex space-x-4">
-            <a
-              href={`https://twitter.com/intent/tweet?url=${encodeURIComponent(pageURL)}&text=${encodeURIComponent(
-                `${player.name} foi um destaque na temporada ${player.league} jogando pelo ${player.club}. O seu market value é de ${player.value}`
-              )}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button className="bg-blue-500 text-white px-4 py-2 rounded"><FaSquareXTwitter /> </button>
-            </a>
-            <a
-              href={`https://wa.me/?&text=${encodeURIComponent(
-                `${player.name} foi um destaque na temporada ${player.league} jogando pelo ${player.club}. O seu market value é de ${player.value}. Veja mais em ${pageURL}`
-              )}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <button className="bg-green-500 text-white px-4 py-2 rounded"><FaWhatsapp /> </button>
-            </a>
-          </div>
-        </main>
+          <svg className="invisible absolute" width="0" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1">
+            <defs>
+              <filter id="rounded">
+                <feGaussianBlur in="SourceGraphic" stdDeviation="9" result="blur" />
+                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo" />
+                <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+              </filter>
+            </defs>
+          </svg>
+        </div>
+        {/* </main> */}
       </div>
     );
   } catch (error) {
@@ -174,3 +187,8 @@ export default async function PlayerDetails({ params }: PlayerDetailsProps) {
     return (notFound())
   }
 }
+
+
+
+
+
