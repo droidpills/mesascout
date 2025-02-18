013b730b5c99742eb871b59d7bdc505dececf1c8
naysaralodi <naysaralodi@gmail.com>
2025-02-18 15:44:47 -0300

    Feat: adjusted internal player page

--- src/app/[season]/[name]/page.tsx
+++ src/app/[season]/[name]/page.tsx
@@ -6,9 +6,9 @@ import { normalizeName } from "../../utils/normalizeName";
 import { normalizeFileName } from "../../utils/normalizeFileName";
 import { FaUserCircle } from "react-icons/fa";
 import { FaRegStarHalfStroke, FaStar } from "react-icons/fa6";
-import { IoChevronBackCircleOutline } from "react-icons/io5";
-import Link from "next/link";
+import { BsChevronCompactLeft } from "react-icons/bs";
 import { FaLink } from "react-icons/fa6";
+import Link from "next/link";
 import React from "react";
 
 const SEASONS_DATA = {
@@ -173,9 +173,9 @@ export default async function PlayerDetails({ params }: PlayerDetailsProps) {
 
         <main className="flex justify-center mx-auto flex-wrap lg:gap-x-10">
           <div className="relative w-[400px] flex items-center justify-center h-full p-5">
-          <div className="absolute -mt-6 -left-3 top-[50%] z-10 lg:pl-0 lg:-left-20">
-          <Link href={`/${season}`} className="px-5 py-2 rounded-full flex items-center text-sm font-medium text-[#48484986] hover:text-[#292C34] hover:bg-[#7a7c80]">
-            <IoChevronBackCircleOutline size={50} className="mr-2" />
+          <div className="absolute -mt-6 -left-8 top-[50%] z-10 lg:pl-0 lg:-left-20">
+          <Link href={`/${season}`} className="rounded-full flex items-center text-sm font-medium text-[#48484986] hover:text-[#292C34] hover:bg-[#7a7c80]">
+            <BsChevronCompactLeft size={50} />
           </Link>
         </div>
 
