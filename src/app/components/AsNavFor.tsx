13a7f0e3372ad5139ae0888ba26038a8ad15c093
naysaralodi <naysaralodi@gmail.com>
2025-02-28 12:00:08 -0300

    Feat: bugs backend part 1

--- src/app/components/AsNavFor.tsx
+++ src/app/components/AsNavFor.tsx
@@ -26,6 +26,16 @@ const AsNavFor: React.FC<AsNavForProps> = ({ players, imageExists, currentSeason
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: currentIndex,
+        },
+      },
+    ],
     initialSlide: visiblePlayers.length > 2 ? 2 : Math.floor(visiblePlayers.length / 2),
     afterChange: (current: number) => handleAfterChange(current),
     className: "center",
