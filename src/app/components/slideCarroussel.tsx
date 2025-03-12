6e101ac6821cf1273090cfeac9fb84ff891420b7
Marco Lima <marco.lima@qr.capital>
2025-03-12 12:45:28 -0300

    feat: emblaslideperview

--- src/app/components/slideCarroussel.tsx
+++ src/app/components/slideCarroussel.tsx
@@ -26,7 +26,7 @@ const SlideCarroussel: React.FC<SlideCarrousselProps> = (props) => {
   const currentIndex = players.findIndex((p) => normalizeName(p.name) === name);
 
   const [emblaRef, emblaApi] = useEmblaCarousel(
-    { loop: false, startIndex: 6, align:'center' })
+    { loop: false, startIndex: currentIndex, align:'center' })
   const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(emblaApi)
   const {
     prevBtnDisabled,
@@ -43,24 +43,41 @@ const SlideCarroussel: React.FC<SlideCarrousselProps> = (props) => {
   if (currentIndex == 0)
     numberPlayersEnd = 14;
 
+  //console.log(currentIndex)
+
   const visiblePlayers = players.slice(Math.max(currentIndex + numberPlayersStart, 0), Math.min(currentIndex + numberPlayersEnd, players.length));
 
   const mostLeftPlayer = players[currentIndex - 8];
   const mostRightPlayer = players[currentIndex + 8];
 
-  const handleEdge = useCallback(() => {
-    if (!emblaApi) return;
+  const handleScroll = useCallback((emblaApi: object) => {
+    //if (!emblaApi) return;
+    const progress = emblaApi.scrollProgress();
 
-    const isAtEnd = emblaApi.canScrollNext() === false;
-    const isAtStart = emblaApi.canScrollPrev() === false;
+    console.log('handlesettle');
 
-    if (isAtEnd && mostRightPlayer) {
+    const index = emblaApi.selectedScrollSnap();
+    const newPlayer = visiblePlayers[index];
+
+    console.log('emblaApicanscroll');
+    console.log(emblaApi);
+
+    //console.log('newPlayer');
+   // console.log(newPlayer);
+
+    window.history.replaceState({}, "", `/${season}/${normalizeName(newPlayer.name)}`);
+
+
+
+    if (emblaApi.canScrollNext() === false) {
       window.location.assign(`/${season}/${normalizeName(mostRightPlayer.name)}`);
-    } else if (isAtStart && mostLeftPlayer) {
+    } else if (emblaApi.canScrollPrev() === false) {
       window.location.assign(`/${season}/${normalizeName(mostLeftPlayer.name)}`);
     }
+
   }, [emblaApi, season, mostLeftPlayer, mostRightPlayer]);
   
+  /*
   const handleAfterChange = useCallback(() => {
     if (!emblaApi) return;
     const index = emblaApi.selectedScrollSnap();
@@ -69,25 +86,52 @@ const SlideCarroussel: React.FC<SlideCarrousselProps> = (props) => {
     if (newPlayer) {
       window.history.replaceState({}, "", `/${season}/${normalizeName(newPlayer.name)}`);
     }
-  }, [emblaApi, season, visiblePlayers]);
+  }, [emblaApi, season, visiblePlayers]);*/
+
+  const SLIDE_COUNT = 5
+  const slides = Array.from(Array(SLIDE_COUNT).keys())
 
   useEffect(() => {
     if (!emblaApi) return;
 
-    emblaApi.on("select", handleAfterChange);
-    emblaApi.on("scroll", handleEdge);
+   // emblaApi.on("select", handleAfterChange);
 
-    return () => {
+    emblaApi.on("settle", handleScroll);
+
+   /* return () => {
       emblaApi.off("select", handleAfterChange);
       emblaApi.off("scroll", handleEdge);
-    };
-  }, [emblaApi, handleAfterChange, handleEdge]);
-
+    };*/
+  }, [emblaApi, handleScroll]);
   return (
+    <div>
+      <section className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+        {visiblePlayers.map((player) => {
+           // console.log(player)
+            const playerImageURL = `https://storage.googleapis.com/mesascout/images/${currentSeason}/${normalizeFileName(
+              player.name,
+              player.previous_club ?? player.club,
+              "player_image"
+            )}.png`;
+            return <PlayerCard key={player.name} player={player} imageExists={imageExists} playerImageURL={playerImageURL} />;
+          })}
+        </div>
+      </div>
+
+
+    </section>
+    </div>
+  );
+ /*
+  return (
+ 
     <div className="embla">
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
           {visiblePlayers.map((player) => {
+           // console.log(player)
             const playerImageURL = `https://storage.googleapis.com/mesascout/images/${currentSeason}/${normalizeFileName(
               player.name,
               player.previous_club ?? player.club,
@@ -117,7 +161,9 @@ const SlideCarroussel: React.FC<SlideCarrousselProps> = (props) => {
         </div>
       </div>
     </div>
+
   );
+  */
 };
 
 export default SlideCarroussel;
